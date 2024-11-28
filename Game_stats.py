80b24bcd3eabb2f8d6c0d897188d9a4e0909640a
JBGuybrush <jbp88@duck.com>
2024-11-28 20:27:50 +0100

    Basket Stats project

--- Game_stats.py
+++ Game_stats.py
@@ -1,56 +1,43 @@
-#Creación de listas
+#Vamos a emplear de las colecciones vistas en clase las listas para esta tarea. A continuación las definimos.
 lista_anotaciones = []
 lista_dorsales = []
 lista_jugadores = []
-maxima_anotacion = 0
 nombre_jugador_maxima_anotacion = ""
 
-for jugador in range(5):
+#Función para la toma de datos del equipo, jugadores y anotaciones.
+def entry(maxima_anotacion=0):
+    for jugador in range(1):
     #solicitar las anotaciones
-    jugador = input(f"introduce el nombre del jugador {jugador}")
-    dorsal = input(f"introduce el dorsal {jugador}")
-    anotacion = int(input(f"introduce anotacion del jugador {jugador}"))
+        jugador = input(f"introduce el nombre del jugador {jugador}")
+        dorsal = input(f"introduce el dorsal {jugador}")
+        anotacion = int(input(f"introduce anotacion del jugador {jugador}"))
     #añadir a una lista
-    lista_jugadores.append(jugador)
-    lista_dorsales.append(dorsal)
-    lista_anotaciones.append(anotacion)
+        lista_jugadores.append(jugador)
+        lista_dorsales.append(dorsal)
+        lista_anotaciones.append(anotacion)
     #el maximo anotador
     if(anotacion>maxima_anotacion):
         maxima_anotacion = anotacion
         nombre_jugador_maxima_anotacion = jugador
 
-#imprimimos
-for i in range (3):
-    print(f"jugador {lista_jugadores[i]}, dorsal [{lista_dorsales[i]}, anotacion [{lista_anotaciones[i]}]")
+#Función para imprimir las colecciones
+def stats(maxima_anotacion=0):
+    for i in range (1):
+        print(f"jugador {lista_jugadores[i]}, dorsal {lista_dorsales[i]}, anotacion [{lista_anotaciones[i]}]")
+        print(f"Máximo anotador es {nombre_jugador_maxima_anotacion} con {maxima_anotacion} puntos.")
 
-print(f"Máximo anotador es {nombre_jugador_maxima_anotacion} con {maxima_anotacion} puntos.")
-
-#cierre o reinicio del programa por interacción con usuario
-si = True
-no = False
-print("¿Quieres salir de python?")
-typed_answer = input("Responda si o no:")
-if typed_answer == "si":
+#Función para el cierre o reinicio del programa por interacción con usuario
+def cierre():
+    reinicio = input("¿Quieres cerrar el programa?")
+    if reinicio == "y" or reinicio == "yes" or reinicio == "si":
         print("Sayonara, baby")
         quit()
-if typed_answer == "no":
-        print("¿Quieres reiniciar el programa?")
-        reinicio = input("y/n:")
-        if reinicio == "y":
-            print("Empecemos de nuevo")
-            for jugador in range(5):
-                # solicitar las anotaciones
-                jugador = input(f"introduce el nombre del jugador {jugador}")
-                dorsal = input(f"introduce el dorsal {jugador}")
-                anotacion = int(input(f"introduce anotacion del jugador {jugador}"))
-                # añadir a una lista
-                lista_jugadores.append(jugador)
-                lista_dorsales.append(dorsal)
-                lista_anotaciones.append(anotacion)
-                # el maximo anotador
-                if (anotacion > maxima_anotacion):
-                    maxima_anotacion = anotacion
-                    nombre_jugador_maxima_anotacion = jugador
-        if reinicio == "n":
-            print("Sayonara, baby")
-            quit()
+    else:
+        print("Okey")
+        entry(maxima_anotacion=0)
+        stats(maxima_anotacion=0)
+        cierre()
+
+entry(maxima_anotacion=0)
+stats(maxima_anotacion=0)
+cierre()
